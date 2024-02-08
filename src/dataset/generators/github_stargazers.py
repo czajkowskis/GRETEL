dd5b435ed97c747ec2e0424108cfeb64c37e37be
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2024-02-08 22:16:17 +0100

    Optimize generator

--- src/dataset/generators/github_stargazers.py
+++ src/dataset/generators/github_stargazers.py
@@ -5,7 +5,7 @@ from src.dataset.instances.graph import GraphInstance
 from src.dataset.generators.base import Generator
 
 class GithubStargazersGenerator(Generator):
-    
+
     def init(self):
         self.data_path = self.local_config['parameters']['data_path']
         self.max_number_nodes = self.local_config['parameters']['max_number_nodes']
@@ -20,35 +20,35 @@ class GithubStargazersGenerator(Generator):
         local_config['parameters']['max_number_nodes'] = local_config['parameters'].get('max_number_nodes', 200)
 
     def generate_dataset(self):
-        if not len(self.dataset.instances):
-            adj_matrix_path = join(self.data_path, 'github_stargazers_A.txt')
-            graph_indicator_path = join(self.data_path, 'github_stargazers_graph_indicator.txt')
-            graph_labels_path = join(self.data_path, 'github_stargazers_graph_labels.txt')
+        if len(self.dataset.instances):
+            return
 
-            edges = np.loadtxt(adj_matrix_path, delimiter=',',dtype=int)
-            graph_indicator = np.loadtxt(graph_indicator_path, dtype=int)
-            graph_labels = np.loadtxt(graph_labels_path, dtype=int)
+        adj_matrix_path = join(self.data_path, 'github_stargazers_A.txt')
+        graph_indicator_path = join(self.data_path, 'github_stargazers_graph_indicator.txt')
+        graph_labels_path = join(self.data_path, 'github_stargazers_graph_labels.txt')
 
-            for graph_id in range(1, len(graph_labels) + 1):
-                print(f"Generating graph {graph_id}")
-                
-                # Filter for the edges of the currnent graph
-                graph_nodes = np.where(graph_indicator == graph_id)[0] + 1 # Add one to make up for the 0th index
-                if len(graph_nodes) > self.max_number_nodes:
-                    print(f"Graph {graph_id} skipped due to large size")
-                    continue # skiping the biggest graphs to optimize needed resoureces
+        edges = np.loadtxt(adj_matrix_path, delimiter=',',dtype=int)
+        graph_indicator = np.loadtxt(graph_indicator_path, dtype=int)
+        graph_labels = np.loadtxt(graph_labels_path, dtype=int)
 
-                graph_edges = edges[np.isin(edges, graph_nodes).any(axis=1)]
+        graph_ids, node_counts = np.unique(graph_indicator, return_counts=True)
+        filtered = np.where(node_counts < self.max_number_nodes)[0]
 
-                G = nx.Graph()
-                G.add_edges_from(graph_edges)
-                graph = nx.to_numpy_array(G)
+        self.context.logger.info(f"Generating {len(filtered)} graphs")
+        for iteration, graph_id in enumerate(graph_ids[filtered], start=1):
 
-                label = graph_labels[graph_id - 1]
-                self.dataset.instances.append(GraphInstance(id=graph_id, data=graph, label=label))    
+            if iteration % 200 == 0:
+                self.context.logger.info(f"Generating graph {iteration} with id {graph_id}")
+
+            # Filter for the edges of the currnent graph
+            node_count = node_counts[graph_id]
+            graph_edges = edges[np.isin(edges, node_count).any(axis=1)]
+
+            G = nx.Graph()
+            G.add_edges_from(graph_edges)
+
+            label = graph_labels[graph_id - 1]
+            self.dataset.instances.append(GraphInstance(id=graph_id, data=nx.to_numpy_array(G), label=label))
 
     def get_num_instances(self):
         return len(self.dataset.instances)
-
-
-
